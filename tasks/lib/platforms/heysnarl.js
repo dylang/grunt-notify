1720045e4e92dc508907074359ab7b0ee8d0b5c3
vohof <foo@bar>
2013-07-14 06:02:10 +0800

    add extra options

--- tasks/lib/platforms/heysnarl.js
+++ tasks/lib/platforms/heysnarl.js
@@ -15,6 +15,8 @@ var findApp = require('../util/findApp');
 var cmd = 'heysnarl';
 var IS_WINDOWS = os.type() === 'Windows_NT';
 var DEFAULT_IMAGE = path.resolve(__dirname + '../../../../images/grunt-logo.png');
+var DEFAULT_TIMEOUT = 2;
+var DEFAULT_PRIORITY = 0;
 
 function isSupported() {
   return IS_WINDOWS && findApp(cmd);
@@ -22,10 +24,16 @@ function isSupported() {
 
 module.exports = isSupported() && function(options, cb) {
 
+  var image = options.image || DEFAULT_IMAGE;
+  var timeout = options.timeout || DEFAULT_TIMEOUT;
+  var priority = options.priority || DEFAULT_PRIORITY;
+
   var param = 'notify?'
             + 'title=' + options.title
             + '&text=' + options.message
-            + '&icon=' + DEFAULT_IMAGE;
+            + '&icon=' + image
+            + '&timeout=' + timeout
+            + '&priority=' + priority;
 
   spawn(cmd, [param], cb);
 };
