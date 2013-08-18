3aa6a83087a624be92710c62c1c64caec96510f0
Dylan Greene <dylang@gmail.com>
2013-08-18 17:24:37 -0400

    another directory option for Snarl users.

--- tasks/lib/platforms/hey-snarl.js
+++ tasks/lib/platforms/hey-snarl.js
@@ -21,15 +21,16 @@ var DEFAULT_IMAGE = path.resolve(__dirname + '../../../../images/grunt-logo.png'
 
 function findInstall() {
 
-  var INSTALL_DIR = path.join('full phat', 'Snarl', 'tools');
+  var INSTALL_DIR_1 = path.join('full phat', 'Snarl', 'tools');
+  var INSTALL_DIR_2 = path.join('Snarl', 'tools');
   var PROGRAM_FILES = process.env.ProgramFiles || '';
   var PROGRAM_FILES_X86 = process.env['ProgramFiles(x86)'] || '';
-  var full_path = path.join(PROGRAM_FILES, INSTALL_DIR, cmd);
-  var full_path_x86 = path.join(PROGRAM_FILES_X86, INSTALL_DIR, cmd);
 
   return findApp(cmd) ||
-    findApp(full_path) ||
-    findApp(full_path_x86);
+    findApp(path.join(PROGRAM_FILES, INSTALL_DIR_1, cmd)) ||
+    findApp(path.join(PROGRAM_FILES_X86, INSTALL_DIR_1, cmd)) ||
+    findApp(path.join(PROGRAM_FILES, INSTALL_DIR_2, cmd)) ||
+    findApp(path.join(PROGRAM_FILES_X86, INSTALL_DIR_2, cmd))
 }
 
 var fullPathToApplication = findInstall();
